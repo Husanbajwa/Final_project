7789e2366a46118817c5d30d198a2a97c78a1e5c
Husanpreet Singh <76039437+Husanbajwa@users.noreply.github.com>
2022-08-15 21:02:09 +0530

    Update urls.py

--- hospital_management/urls.py
+++ hospital_management/urls.py
@@ -1,5 +1,4 @@
 """hospital_management URL Configuration
-error
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
